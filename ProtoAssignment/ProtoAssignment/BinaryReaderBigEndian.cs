b73d45e49aae310b8cbb4eb4bc76ddf64a680c9a
John Steele Urban <jurban@pagregion.com>
2017-03-09 17:18:39 -0700

    Commit Project

---
+++ ProtoAssignment/ProtoAssignment/BinaryReaderBigEndian.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ProtoAssignment
+{
+   public class BinaryReaderBigEndian : BinaryReader
+    {
+        public BinaryReaderBigEndian(Stream input) : base(input)
+        {
+        }
+
+        public BinaryReaderBigEndian(Stream input, Encoding encoding) : base(input, encoding)
+        {
+        }
+
+        public BinaryReaderBigEndian(Stream input, Encoding encoding, bool leaveOpen) : base(input, encoding, leaveOpen)
+        {
+        }
+
+        public override byte[] ReadBytes(int count)
+        {
+            var reverse = base.ReadBytes(count);
+            Array.Reverse(reverse);
+            return reverse;
+        }
+
+        public UInt32 ReadConvertToUInt32(int i, int i1)
+        {
+            return BitConverter.ToUInt32(ReadBytes(i), i1);
+        }
+
+        public Mps7.RecordType ConvertRecType(int i)
+        {
+            return (Mps7.RecordType)Int32.Parse(BitConverter.ToString(ReadBytes(i)));
+        }
+
+        public ulong ReadConvertToUInt64(int i, int i1)
+        {
+            return BitConverter.ToUInt64(ReadBytes(i), i1);
+        }
+
+        public double ReadDollars(int i, int i1)
+        {
+            return BitConverter.ToDouble(ReadBytes(8), 0);
+        }
+    }
+}
---
+++ ProtoAssignment/ProtoAssignment/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ProtoAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Mps7().Start();
+        }
+    }
+
+    public class Mps7
+    {
+        #region private vars
+        private string _mainframe;
+        private string _version;
+        private int _numRecords;
+        #endregion private vars
+
+        #region properties
+        public double TotalDebits { get; set; } = 0.0;
+        public double TotalCredits { get; set; } = 0.0;
+        public int TotalAutopayStarted { get; set; } = 0;
+        public int TotalAutopayEnded { get; set; } = 0;
+        public double SpecialUserBalance { get; set; } = 0;
+        public enum RecordType
+        {
+            Debit = 0,
+            Credit = 1,
+            StartAutopay = 2,
+            EndAutopay = 3
+        }
+        #endregion properties
+
+        #region parsing
+        public void Start()
+        {
+            using (BinaryReaderBigEndian bigEndianReader = new BinaryReaderBigEndian(File.Open("txnlog.dat", FileMode.Open)))
+            {
+                _mainframe = Encoding.Default.GetString(bigEndianReader.ReadBytes(4));
+                _version = "v" + BitConverter.ToString(bigEndianReader.ReadBytes(1));
+                _numRecords = (int)bigEndianReader.ReadConvertToUInt32(4, 0);
+
+                for (int i = 0; i <= _numRecords; i++)
+                {
+
+                    RecordType recType = bigEndianReader.ConvertRecType(1);
+                    var timeStamp = (int)bigEndianReader.ReadConvertToUInt32(4, 0);
+                    var userId = (long)bigEndianReader.ReadConvertToUInt64(8, 0);
+                    bool isSpecialUser = userId == 2456938384156277127;
+                    if (isSpecialUser)
+                    {
+                        var bla = "";
+                    }
+                    double cash;
+                    switch (recType)
+                    {
+                        case RecordType.Debit:
+                            cash = bigEndianReader.ReadDollars(8, 0);
+                            TotalDebits += cash;
+                            SpecialUserBalance += isSpecialUser ? cash : 0.0;
+                            break;
+                        case RecordType.Credit:
+                            cash = bigEndianReader.ReadDollars(8, 0);
+                            TotalCredits += cash;
+                            SpecialUserBalance += isSpecialUser ? cash : 0.0;
+                            break;
+                        case RecordType.StartAutopay:
+                            TotalAutopayStarted++;
+                            break;
+                        case RecordType.EndAutopay:
+                            TotalAutopayEnded++;
+                            break;
+                    }
+                }
+            }
+            #endregion parsing
+
+            Console.WriteLine(_mainframe + " " + _version + " Number of Records: " + _numRecords);
+            Console.WriteLine();
+            Console.WriteLine("* What is the total amount in dollars of debits?");
+            Console.WriteLine("========>   " + TotalDebits.ToString("C"));
+            Console.WriteLine();
+            Console.WriteLine("* What is the total amount in dollars of credits?");
+            Console.WriteLine("========>   " + TotalCredits.ToString("C"));
+            Console.WriteLine();
+            Console.WriteLine("* How many autopays were started?");
+            Console.WriteLine("========>   " + TotalAutopayStarted);
+            Console.WriteLine();
+            Console.WriteLine("* How many autopays were ended?");
+            Console.WriteLine("========>   " + TotalAutopayEnded);
+            Console.WriteLine("* What is balance of user ID 2456938384156277127?");
+            Console.WriteLine("========>   " + SpecialUserBalance.ToString("C"));
+            Console.ReadLine();
+        }
+    }
+}
